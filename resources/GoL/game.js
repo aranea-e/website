5db6791b82f7e895144b952d8694fd91dca403a9
Ian Perry <60653858+aranea-e@users.noreply.github.com>
2026-01-22 18:26:37 -0500

    Digital Game Design Toy Project

---
+++ resources/GoL/game.js
@@ -0,0 +1,408 @@
+/*
+game.js for Perlenspiel 3.3.x
+Last revision: 2022-03-15 (BM)
+
+Perlenspiel is a scheme by Professor Moriarty (bmoriarty@wpi.edu).
+This version of Perlenspiel (3.3.x) is hosted at <https://ps3.perlenspiel.net>
+Perlenspiel is Copyright © 2009-22 Brian Moriarty.
+This file is part of the standard Perlenspiel 3.3.x devkit distribution.
+
+Perlenspiel is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Perlenspiel is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You may have received a copy of the GNU Lesser General Public License
+along with the Perlenspiel devkit. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+This JavaScript file is a template for creating new Perlenspiel 3.3.x games.
+Any unused event-handling function templates can be safely deleted.
+Refer to the tutorials and documentation at <https://ps3.perlenspiel.net> for details.
+*/
+
+/*
+The following comment lines are for JSHint <https://jshint.com>, a tool for monitoring code quality.
+You may find them useful if your development environment is configured to support JSHint.
+If you don't use JSHint (or are using it with a configuration file), you can safely delete these two lines.
+*/
+
+/* jshint browser : true, devel : true, esversion : 6, freeze : true */
+/* globals PS : true */
+
+"use strict"; // Do NOT remove this directive!
+
+/*
+PS.init( system, options )
+Called once after engine is initialized but before event-polling begins.
+This function doesn't have to do anything, although initializing the grid dimensions with PS.gridSize() is recommended.
+If PS.grid() is not called, the default grid dimensions (8 x 8 beads) are applied.
+Any value returned is ignored.
+[system : Object] = A JavaScript object containing engine and host platform information properties; see API documentation for details.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+let tickrate = 60;
+let age = 0;
+let timer = null;
+let tiles = null;
+let numtiles = null;
+
+let draw = [true, false];
+
+PS.init = function( system, options ) {
+	// Uncomment the following code line
+	// to verify operation:
+
+	// PS.debug( "PS.init() called\n" );
+
+	// This function should normally begin
+	// with a call to PS.gridSize( x, y )
+	// where x and y are the desired initial
+	// dimensions of the grid.
+	// Call PS.gridSize() FIRST to avoid problems!
+	// The sample call below sets the grid to the
+	// default dimensions (8 x 8).
+	// Uncomment the following code line and change
+	// the x and y parameters as needed.
+
+	PS.gridSize( 8, 8 );
+
+	// This is also a good place to display
+	// your game title or a welcome message
+	// in the status line above the grid.
+	// Uncomment the following code line and
+	// change the string parameter as needed.
+
+	initializeGrid();
+	setTickRate( tickrate );
+	tick();
+	startGame();
+
+	// Add any other initialization code you need here.
+};
+
+/*
+PS.touch ( x, y, data, options )
+Called when the left mouse button is clicked over bead(x, y), or when bead(x, y) is touched.
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.touch = function( x, y, data, options ) {
+	// Uncomment the following code line
+	// to inspect x/y parameters:
+
+	// PS.debug( "PS.touch() @ " + x + ", " + y + "\n" );
+
+	// Add code here for mouse clicks/touches
+	// over a bead.
+
+	draw = [!tiles[x][y], true];
+	// PS.debug(draw);
+	set(x, y, draw[0]);
+};
+
+/*
+PS.release ( x, y, data, options )
+Called when the left mouse button is released, or when a touch is lifted, over bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.release = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.release() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse button/touch is released over a bead.
+
+	draw[1] = false;
+};
+
+/*
+PS.enter ( x, y, button, data, options )
+Called when the mouse cursor/touch enters bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.enter = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.enter() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse cursor/touch enters a bead.
+	if (draw[1]) {
+		set(x, y, draw[0]);
+	}
+};
+
+/*
+PS.exit ( x, y, data, options )
+Called when the mouse cursor/touch exits bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.exit = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.exit() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse cursor/touch exits a bead.
+};
+
+/*
+PS.exitGrid ( options )
+Called when the mouse cursor/touch exits the grid perimeter.
+This function doesn't have to do anything. Any value returned is ignored.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.exitGrid = function( options ) {
+	// Uncomment the following code line to verify operation:
+
+	// PS.debug( "PS.exitGrid() called\n" );
+
+	// Add code here for when the mouse cursor/touch moves off the grid.
+
+	draw[1] = false;
+};
+
+/*
+PS.keyDown ( key, shift, ctrl, options )
+Called when a key on the keyboard is pressed.
+This function doesn't have to do anything. Any value returned is ignored.
+[key : Number] = ASCII code of the released key, or one of the PS.KEY_* constants documented in the API.
+[shift : Boolean] = true if shift key is held down, else false.
+[ctrl : Boolean] = true if control key is held down, else false.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.keyDown = function( key, shift, ctrl, options ) {
+	// Uncomment the following code line to inspect first three parameters:
+
+	// Add code here for when a key is pressed.
+
+	// PS.debug( "PS.keyDown(): key=" + key + ", shift=" + shift + ", ctrl=" + ctrl + "\n" );
+
+	if (key === 43 || key === 61) { // '+' key
+		setTickRate( tickrate + 1 );
+	} else if (key === 173) { // '-' key
+		setTickRate( tickrate - 1 );
+	}
+	
+	if (key === 111 || key === 97) { // 'o' key
+		PS.gridSize(PS.gridSize().width - 1, PS.gridSize().height);
+		initializeGrid();
+		age = 0;
+		render();
+	} else if (key === 112 || key === 80) { // 'p' key
+		PS.gridSize( PS.gridSize().width + 1, PS.gridSize().height );
+		initializeGrid();
+		age = 0;
+		render();
+	}
+	
+	if (key === 91 || key === 123) { // '{' key
+		PS.gridSize(PS.gridSize().width, PS.gridSize().height - 1);
+		initializeGrid();
+		age = 0;
+		render();
+	} else if (key === 93 || key === 125) { // '}' key
+		PS.gridSize( PS.gridSize().width, PS.gridSize().height + 1 );
+		initializeGrid();
+		age = 0;
+		render();
+	}
+	
+	if (key === 120 || key === 88) { // 'x' key
+		initializeGrid();
+		age = 0;
+		render();
+	}
+
+	stopGame();
+};
+
+/*
+PS.keyUp ( key, shift, ctrl, options )
+Called when a key on the keyboard is released.
+This function doesn't have to do anything. Any value returned is ignored.
+[key : Number] = ASCII code of the released key, or one of the PS.KEY_* constants documented in the API.
+[shift : Boolean] = true if shift key is held down, else false.
+[ctrl : Boolean] = true if control key is held down, else false.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.keyUp = function( key, shift, ctrl, options ) {
+	// Uncomment the following code line to inspect first three parameters:
+
+	// PS.debug( "PS.keyUp(): key=" + key + ", shift=" + shift + ", ctrl=" + ctrl + "\n" );
+
+	// Add code here for when a key is released.
+
+	startGame();
+};
+
+/*
+PS.input ( sensors, options )
+Called when a supported input device event (other than those above) is detected.
+This function doesn't have to do anything. Any value returned is ignored.
+[sensors : Object] = A JavaScript object with properties indicating sensor status; see API documentation for details.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+NOTE: Currently, only mouse wheel events are reported, and only when the mouse cursor is positioned directly over the grid.
+*/
+
+PS.input = function( sensors, options ) {
+	// Uncomment the following code lines to inspect first parameter:
+
+//	 var device = sensors.wheel; // check for scroll wheel
+//
+//	 if ( device ) {
+//	   PS.debug( "PS.input(): " + device + "\n" );
+//	 }
+
+	// Add code here for when an input event is detected.
+};
+
+function setTickRate( x ) {
+	if (x > 0 && x <= 600) {
+		tickrate = x;
+		updateStatus();
+		return x;
+	}
+	return -1;
+}
+
+function stopGame() {
+	if (timer !== null)
+		PS.timerStop(timer);
+	timer = null;
+}
+
+function startGame() {
+	if (timer !== null)
+		PS.timerStop(timer);
+	timer = PS.timerStart( tickrate, tick );
+}
+
+function tick() {
+	age++;
+	simulate();
+	render();
+}
+
+function updateStatus() {
+	PS.statusText( "Tickrate: " + tickrate + " | +/- to change | Age: " + age );
+}
+
+function set(x, y, value) {
+	tiles[x][y] = value;
+	render();
+}
+
+function render() {
+	for (let i = 0; i < tiles.length; i++) {
+		const element = tiles[i];
+		for (let j = 0; j < element.length; j++) {
+			const tile = element[j];
+			if (tile === false) {
+				PS.color( i, j, PS.COLOR_WHITE);
+			} else {
+				PS.color( i, j, PS.COLOR_BLACK);
+			}
+		}
+	}
+
+	updateStatus();
+}
+
+function simulate() {
+	updateAdjacent();
+	updateGrid();
+}
+
+function updateAdjacent() {
+	for (let i = 0; i < tiles.length; i++) {
+		const element = tiles[i];
+		for (let j = 0; j < element.length; j++) {
+			const tile = element[j];
+			numtiles[i][j] = countAdjacent(i, j);
+		}
+	}
+}
+
+function countAdjacent(x, y) {
+	let count = 0;
+	let leftx = x - 1;
+	if (leftx < 0) leftx = PS.gridSize().width - 1;
+	if (leftx > PS.gridSize().width - 1) leftx = 0;
+	let rightx = x + 1;
+	if (rightx < 0) rightx = PS.gridSize().width - 1;
+	if (rightx > PS.gridSize().width - 1) rightx = 0;
+	let upy = y - 1;
+	if (upy < 0) upy = PS.gridSize().height - 1;
+	if (upy > PS.gridSize().height - 1) upy = 0;
+	let downy = y + 1;
+	if (downy < 0) downy = PS.gridSize().height - 1;
+	if (downy > PS.gridSize().height - 1) downy = 0;
+
+	if (tiles[leftx][upy]) count++;
+	if (tiles[x][upy]) count++;
+	if (tiles[rightx][upy]) count++;
+	if (tiles[leftx][y]) count++;
+	if (tiles[rightx][y]) count++;
+	if (tiles[leftx][downy]) count++;
+	if (tiles[x][downy]) count++;
+	if (tiles[rightx][downy]) count++;
+
+	return count;
+}
+
+function updateGrid() {
+	for (let i = 0; i < tiles.length; i++) {
+		const element = tiles[i];
+		for (let j = 0; j < element.length; j++) {
+			const tile = element[j];
+			updateTile(i, j);
+		}
+	}
+}
+
+function updateTile(x, y) {
+	let count = numtiles[x][y];
+	if (tiles[x][y]) {
+		if (count < 2 || count > 3) {
+			tiles[x][y] = false;
+		}
+	} else {
+		if (count === 3) {
+			tiles[x][y] = true;
+		}
+	}
+}
+
+function initializeGrid() {
+	tiles = Array.from({ length: PS.gridSize().width }, () => new Array(PS.gridSize().height).fill(false));
+	numtiles = Array.from({ length: PS.gridSize().width }, () => new Array(PS.gridSize().height).fill(0));
+}
